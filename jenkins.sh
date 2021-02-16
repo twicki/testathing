9dcce469815688b3b50a78179754516cae5c12e6
tobias <twicki12@gmail.com>
2021-02-16 19:27:09 +0100

    create no longer nececssary

--- jenkins.sh
+++ jenkins.sh
@@ -7,7 +7,7 @@ source test_ve/bin/activate
 export MODULEPATH=${MODULEPATH}:/project/s1053/install/modulefiles
 module load ccache/4.2
 cd ..
-create_ccache /scratch/snx3000/tobwi/cache_test/ccache
+# create_ccache /scratch/snx3000/tobwi/cache_test/ccache
 source /scratch/snx3000/tobwi/cache_test/ccache/bin/activate_ccache
 export CCACHE_BASEDIR=`pwd`
 time python file.py
